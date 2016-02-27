96a3095478b35fd31daf2adf6e2869aa5028160d
Patrick Haikal <pathaikal@gmail.com>
2016-02-27 22:03:23 +1100

    Improved the code and updated the file structure

--- Assets/Scripts/Game 1/GameManager.cs
+++ Assets/Scripts/Game 1/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
+    public float gameLength;
     public List<GameObject> dispensers;
 
     void Start()
@@ -21,22 +22,14 @@ public class GameManager : MonoBehaviour
             dispenser.maxTime = 1.5f;
         }
 
-        StartCoroutine("Countdown");
-    }
-
-    IEnumerator Countdown()
-    {
-        // TODO: handle the count down timer here
-        yield return new WaitForSeconds(3.0f);
-
-        StartCoroutine(Play());
+        StartCoroutine(Countdown.Instance.StartCountdown(Play));
     }
 
     IEnumerator Play()
     {
         dispensers[0].GetComponent<Dispenser>().StartDispenser();
 
-        yield return new WaitForSeconds(10.0f);
+        yield return new WaitForSeconds(gameLength);
 
         dispensers[0].GetComponent<Dispenser>().StopDispenser();
 
---
+++ Assets/Scripts/Utility/Countdown.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Countdown : Singleton<Countdown>
+{
+    public delegate IEnumerator PlayDelegate();
+
+    public IEnumerator StartCountdown(PlayDelegate func)
+    {
+        // TODO: Show the countdown text from here
+        Debug.Log("3");
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("2");
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("1");
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("GO");
+
+        StartCoroutine(func());
+    }
+}
