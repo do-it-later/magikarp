e50fa99888000c13c0d431753910ba9ab2ae4b95
Joseph Tran <valrigardian@gmail.com>
2016-03-01 21:19:21 +1100

    Add initial player manager

---
+++ Assets/Scripts/Utility/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player
+{
+    private int controllerNumber;
+    public int ControllerNumber { get { return controllerNumber; } }
+
+    private int score;
+    public int Score { get { return score;  } }
+
+    private bool isHuman;
+    public bool IsHuman
+    {
+        get { return isHuman; }
+        set { isHuman = value; }
+    }
+
+    public Player(int controller)
+    {
+        controllerNumber = controller;
+        score = 0;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+    }
+
+    public void DecreaseScore()
+    {
+        score--;
+    }
+}
---
+++ Assets/Scripts/Utility/PlayerManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerManager : MonoBehaviour {
+
+    public static PlayerManager instance;
+
+    private List<Player> playerList = new List<Player>();
+    public bool debugMode;
+
+    void Awake()
+    {
+        // Persistent singleton
+        if( instance == null )
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.Log("Duplicate instance detected, destroying duplicate PlayerManager.");
+            Destroy(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        // Add the 4 players
+        for (int i = 1; i <= 4; ++i)
+        {
+            Player p = new Player(i);
+            playerList.Add(p);
+        }
+
+        if(debugMode)
+        {
+            SetHumanPlayer(1);
+        }
+	}
+	
+	public bool SetHumanPlayer(int controller)
+    {
+        var player = FindPlayer(controller);
+
+        // Only the first 4 controllers should be found
+        if (FindPlayer(controller) == null)
+            return false;
+
+        player.IsHuman = true;
+        return true;
+    }
+
+    public bool UnsetHumanPlayer(int controller)
+    {
+        var player = FindPlayer(controller);
+
+        // Only the first 4 controllers should be found
+        if (FindPlayer(controller) == null)
+            return false;
+
+        player.IsHuman = false;
+        return true;
+    }
+
+    public Player FindPlayer(int controller)
+    {
+        foreach (Player p in playerList)
+        {
+            if (p.ControllerNumber == controller)
+            {
+                return p;
+            }
+        }
+
+        return null;
+    }
+}
