4bcc9a66849f9eae04e5379b58d84419eba749fa
Joseph Tran <valrigardian@gmail.com>
2016-03-21 23:22:18 +1100

    Begin Reflex

---
+++ Assets/Scripts/Reflex/ReflexGameManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReflexGameManager : MonoBehaviour {
+
+    private struct Target
+    {
+        public string letter;
+        public Sprite sprite;
+    }
+
+    public List<GameObject> targetObjs;
+    public List<Sprite> sprites;
+
+    private int numRounds = 10;
+    private List<Target> roundTargets = new List<Target>();
+
+    // Use this for initialization
+    void Start()
+    {
+        List<string> letters = new List<string> { "A", "B", "X", "Y" };
+        for(int i = 0; i < letters.Count; ++i)
+        {
+            Target t;
+            t.sprite = sprites[i];
+            t.letter = letters[i];
+            roundTargets.Add(t);
+        }
+
+        StartCoroutine(Countdown.instance.StartCountdown(Play));
+    }
+
+    IEnumerator Play()
+    {
+        int currentRound = 1;
+
+        while (currentRound <= numRounds)
+        {
+            Debug.Log("Round " + currentRound.ToString());
+            SetupNewRound();
+
+            yield return new WaitForSeconds(2.0f);
+
+            StartRound();
+
+            yield return new WaitForSeconds(5.0f);
+
+            currentRound++;
+        }
+    }
+
+    private void StartRound()
+    {
+        foreach( var obj in targetObjs )
+        {
+            var sr = obj.GetComponent<SpriteRenderer>();
+            sr.enabled = true;
+        }
+    }
+
+    private void SetupNewRound()
+    {
+        roundTargets.ShuffleList();
+
+        for(int i = 0; i < roundTargets.Count; ++i)
+        {
+            var obj = targetObjs[i];
+            var sr = obj.GetComponent<SpriteRenderer>();
+            sr.sprite = roundTargets[i].sprite;
+            sr.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Reflex/ReflexPlayer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflexPlayer : MonoBehaviour {
+
+    public int score;
+
+    private bool canPressButton;
+    public bool CanPressButton
+    {
+        get { return canPressButton; }
+        set { canPressButton = value; }
+    }
+
+    private bool wasButtonPressed;
+    private string buttonPressedString;
+    public string ButtonPressedString
+    {
+        get { return buttonPressedString; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        ResetButtons();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if( canPressButton && !wasButtonPressed )
+        {
+            //TODO: Proper mapping to controller
+            if( Input.GetKeyDown(KeyCode.A) )
+            {
+                SetButtonPressed("A");
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                SetButtonPressed("W");
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                SetButtonPressed("S");
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                SetButtonPressed("D");
+            }
+        }
+	}
+
+    public void ResetButtons()
+    {
+        wasButtonPressed = false;
+        buttonPressedString = "";
+    }
+
+    private void SetButtonPressed(string key)
+    {
+        wasButtonPressed = true;
+        buttonPressedString = key;
+    }
+}
