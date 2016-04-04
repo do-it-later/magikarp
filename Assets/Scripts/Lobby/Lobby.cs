f948b901ba1db5a1f9f86b3b05c9db19ae9fd433
Joseph Tran <valrigardian@gmail.com>
2016-04-05 02:01:02 +1000

    Add basic lobby

---
+++ Assets/Scripts/Lobby/Lobby.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Lobby : MonoBehaviour {
+
+    public List<Text> typeTexts;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int p = 1; p <= 4; ++p)
+        {
+            if (Input.GetKeyDown(InputHelper.instance.DidControllerButtonGetPressed(p, InputHelper.Button.A)))
+            {
+                PlayerManager.instance.SetHumanPlayer(p);
+            }
+
+            if (Input.GetKeyDown(InputHelper.instance.DidControllerButtonGetPressed(p, InputHelper.Button.B)))
+            {
+                PlayerManager.instance.UnsetHumanPlayer(p);
+            }
+
+            if( PlayerManager.instance.FindPlayer(p).IsHuman )
+            {
+                typeTexts[p - 1].text = "PLAYER";
+            }
+            else
+            {
+                typeTexts[p - 1].text = "AI";
+            }
+        }
+    }
+}
