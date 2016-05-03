dd31e8fe117b3aca6d8e62cfa5fb85b7f1520669
Patrick Haikal <pathaikal@gmail.com>
2016-05-03 19:47:00 +1000

    Added a sound manager and included it into all the scenes

---
+++ Assets/Scripts/Utility/SoundManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    #region Fields / Properties
+    public AudioSource sfxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+    #endregion
+
+    #region MonoBehavior
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else if(instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    #region Public
+    public void PlaySingleSfx(AudioClip clip)
+    {
+        sfxSource.clip = clip;
+        sfxSource.loop = false;
+        sfxSource.Play();
+    }
+
+    public void PlayLoopedSfx(AudioClip clip)
+    {
+        sfxSource.clip = clip;
+        sfxSource.loop = true;
+        sfxSource.Play();
+    }
+
+    public void StopSfx()
+    {
+        sfxSource.Stop();
+    }
+
+    public void MuteSfx(bool mute)
+    {
+        sfxSource.mute = mute;
+    }
+
+    public bool IsSfxMute()
+    {
+        return sfxSource.mute;
+    }
+
+    public void PlaySingleMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.loop = false;
+        musicSource.Play();
+    }
+
+    public void PlayLoopedMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    public void MuteMusic(bool mute)
+    {
+        musicSource.mute = mute;
+    }
+
+    public bool IsMusicMute()
+    {
+        return musicSource.mute;
+    }
+
+    #endregion
+}
